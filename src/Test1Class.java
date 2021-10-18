5253b729ebd3a1f47987c9985c78368f5385b6a1
ibrahimdemirfr <67127448+ibrahimdemirfr@users.noreply.github.com>
2021-10-19 00:58:31 +0200

    Update Test1Class.java

--- src/Test1Class.java
+++ src/Test1Class.java
@@ -1,5 +1,6 @@
 public class Test1Class {
     public static void main(String[] args) {
         System.out.println("GIT den MERHABA!");
+        System.out.println("GITHUP dan MERHABA!");
     }
 }
